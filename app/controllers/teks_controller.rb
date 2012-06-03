4c0390bed1796197bce32c613bcab4ab213fd77e
tvu <tvu@homeaway.com>
2012-06-03 12:26:58 -0500

    Fix search bug

--- app/controllers/teks_controller.rb
+++ app/controllers/teks_controller.rb
@@ -4,8 +4,8 @@ class TeksController < ApplicationController
     query = params[:search]
     teks = Search.search({description: query, keywords: query}, false).map {|s| s.teks }
     teks.delete_if do |t|
-      if  params[:grade]
-        t.grade.id != params[:grade]
+      if params[:grade]
+        not t.grade.id == params[:grade].to_i
       else
         false
       end
@@ -13,13 +13,13 @@ class TeksController < ApplicationController
 
     teks.delete_if do |t|
       if  params[:subject]
-        t.subject.id != params[:subject]
+        t.subject.id != params[:subject].to_i
       else
         false
       end
     end
 
-		render json:  teks
+		render json: teks
   end
 
 
