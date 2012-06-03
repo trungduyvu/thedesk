b91e5e85d2b46513d3877694dd6907830fe00c11
Linh Vo <hailinh.vo@gmail.com>
2012-06-03 10:05:32 -0500

    add seaches view

--- db/migrate/20120603141855_add_fulltext_search.rb
+++ db/migrate/20120603141855_add_fulltext_search.rb
@@ -1,8 +1,16 @@
 class AddFulltextSearch < ActiveRecord::Migration
-  def up
+  def self.up
+    ActiveRecord::Base.connection.execute<<-SQL
+    CREATE VIEW searches AS
+      SELECT teks.id AS teks_id, teks.description AS teks_description,'' AS teks_keyword
+      FROM teks
+    SQL
 
   end
 
-  def down
+  def self.down
+    ActiveRecord::Base.connection.execute<<-SQL
+    DROP VIEW searches
+    SQL
   end
 end
