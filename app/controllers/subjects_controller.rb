1bbc6b99470d1da04e4af14d08bbad5410644cc6
Michael Yao <myao@homeaway.com>
2012-06-02 19:01:55 -0500

    added teks endpoint, search subjects by grade

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,16 @@
 
 class SubjectsController < ApplicationController
 	def index
-		@subjects = Subject.all
-		render :json => @subjects
+		if g = params[:grade]
+			grade = Grade.find(g)
+			m = {}
+			grade.teks.each do |t| 
+				m[t.subject.name] = t.subject
+			end
+			@subjects = m.values
+		else
+			@subjects = Subject.all			
+		end
+		render json: @subjects
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Thedesk::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  resources :grades, :only => :index
-  resources :subjects, :only => :index
+  resources :grades, only: :index
+  resources :subjects, only: :index
+  resources :teks, only: :index
 end
